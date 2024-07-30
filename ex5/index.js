2b92abbb74193bad525ed59f2b3c045c615e1aca
Ng-HuuDev <nguyenhuudev0203@gmail.com>
2024-07-31 01:53:14 +0700

    AD

---
+++ ex5/index.js
@@ -0,0 +1,22 @@
+//
+
+function sapXep() {
+  var stn = Number(document.getElementById("sot1").value);
+  var sth = Number(document.getElementById("sot2").value);
+  var stb = Number(document.getElementById("sot3").value);
+
+  // output
+
+// xu ly
+
+if (stn % 2 == 0){
+    var stNhat= "so chan"
+}
+else {
+    
+}
+
+  //   Hien ra man hinh
+
+  document.getElementById("result").innerHTML =
+}
